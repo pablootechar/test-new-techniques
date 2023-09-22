03bd3a66c4889752f6c22c398356bd5f766b243d
Pablo Otechar <pablomoraesotechar15@gmail.com>
2023-09-22 17:56:31 -0300

    slakkkkkkkkkkk

--- src/pages/Error/ErrorPage.js
+++ src/pages/Error/ErrorPage.js
@@ -1,10 +1,20 @@
 
 
 export const ErrorPage = () => {
+    const style = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+        textAlign: "center",
+        fontSize: "25px",
+        padding: "20px"
+    }
 
     return (
-        <div>
-            
+        <div style={style}>
+            vish em brother, tentou mas n arrumou nada, ai é paia em...
         </div>
     )
 }
